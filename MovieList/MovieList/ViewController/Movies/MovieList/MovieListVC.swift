a13dc6c59a6d10f3c16876db79ef8d2e08aa24fc
Burak Şentürk <burak.senturk@appcent.mobi>
2019-10-16 20:21:35 +0300

    - Initial commit.

---
+++ MovieList/MovieList/ViewController/Movies/MovieList/MovieListVC.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListVC.swift
+//  MovieList
+//
+//  Created by Burak Şentürk on 16.10.2019.
+//  Copyright © 2019 Burak Şentürk. All rights reserved.
+//
+
+import UIKit
+
+class MovieListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MovieList/MovieList/ViewController/Movies/MovieList/MovieListViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListViewModel.swift
+//  MovieList
+//
+//  Created by Burak Şentürk on 16.10.2019.
+//  Copyright © 2019 Burak Şentürk. All rights reserved.
+//
+
+import Foundation
